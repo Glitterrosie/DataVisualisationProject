c97696cf070b8a2fc45602997c0e3430cc8afef0
Rosmarie Debski <64656859+Glitterrosie@users.noreply.github.com>
2025-09-19 11:25:58 +0200

    first setup

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # DataVisualisationProject
+
+
+# how to activate venv and install dependencies https://www.reddit.com/r/learnpython/comments/m3exau/setting_up_first_python_project_environment/
+# how to add new dependencies
+# 
+ venv/scripts/activate
+2. run streamlit hello to test
+
+streamlit run src/main.py
+
+https://doodleclouds.medium.com/using-github-pages-to-host-your-streamlit-app-f274cbe3b3af
---
+++ src/main.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+
+st.title("Chart Example")
+
+fig, ax = plt.subplots()
+
+fruits = ['apple', 'blueberry', 'cherry', 'orange']
+counts = [40, 100, 30, 55]
+bar_labels = ['red', 'blue', '_red', 'orange']
+bar_colors = ['tab:red', 'tab:blue', 'tab:red', 'tab:orange']
+
+ax.bar(fruits, counts, label=bar_labels, color=bar_colors)
+
+ax.set_ylabel('fruit supply')
+ax.set_title('Fruit supply by kind and color')
+ax.legend(title='Fruit color')
+
+plt.show()
